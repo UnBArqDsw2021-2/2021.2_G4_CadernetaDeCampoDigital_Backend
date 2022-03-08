9e11c297429de2e208126157209d077fd181d2f2
Thiago Paiva <thiagohdaqw@gmail.com>
2022-03-08 13:12:59 -0300

    #107 Arrumando testes

--- core/tests/mixin.py
+++ core/tests/mixin.py
@@ -4,6 +4,7 @@ from usuario.tests import recipes
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
+import uuid
 
 class APITestMixin:
 
@@ -12,10 +13,12 @@ class APITestMixin:
 
     # Caso deseje alterar o nome, CPF, entre outros dados é
     # possível alterar esses atributos
+    idUsuario = uuid.uuid4()
     nome = 'Nome para Teste'
     cpf = '03700076037'
     tipo = Usuario.PRODUTOR
     password = '12345678'
+    telefone = '5599999999999'
     usuario_kwargs = {}
 
 
@@ -25,12 +28,13 @@ class APITestMixin:
 
     def get_client(self):
         client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.get_header_credencial())
+        # client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.get_header_credencial())
         return client
 
     def setUp(self):
         self.user = recipes.usuario.make(
-            nome=self.nome, cpf=self.cpf, tipo=self.tipo,
-            password=self.password, **self.usuario_kwargs
+            idUsuario=self.idUsuario, nome=self.nome, cpf=self.cpf, tipo=self.tipo,
+            password=self.password, telefone=self.telefone, **self.usuario_kwargs
         )
+        self.user.id = self.user.idUsuario
         self.client = self.get_client()
--- produtor/tests/views/tests_produtor.py
+++ produtor/tests/views/tests_produtor.py
@@ -1,7 +1,5 @@
 from core.tests.mixin import APITestMixin
 
-import datetime
-
 from django.test import TestCase
 
 from produtor.models.produtor import Produtor
@@ -10,33 +8,31 @@ from rest_framework.reverse import reverse_lazy
 
 
 class ProdutorAPIViewTest(APITestMixin, TestCase):
-    url = reverse_lazy('produtor-create')
-
-    def setUp(self):
-        pass
+    url = reverse_lazy("produtor-create")
 
     def _payload(self):
         return {
-            'usuario': {
-                'cpf': '78299002052',
-                'dataNascimento': datetime.date(2000, 1, 1),
-                'telefone': '5561912345678',
-                'nome': 'Jailson Mendes',
-                'senha': 'senha_super_secreta'
+            "usuario": {
+                "cpf": "78299002052",
+                "dataNascimento": "2000-01-01",
+                "telefone": "5561912345678",
+                "nome": "Jailson Mendes",
+                "senha": "senha_super_secreta",
             },
-            'dap': 'c5Pnio21'
+            "dap": "c5Pnio21",
         }
 
     def test_cria_produtor(self):
         payload = self._payload()
-        response = self.client.post(self.url, data=payload)
-        self.assertEqual(response.status_code, 200, response.json())
-        self.assertIsNone(response.json.get('senha'))
+
+        response = self.client.post(self.url, payload, format="json")
+        self.assertEqual(response.status_code, 201, response.json())
+        self.assertIsNone(response.json().get("senha"))
         self.assertEqual(Produtor.objects.count(), 1)
 
         prod = Produtor.objects.first()
-        self.assertEqual(prod.usuario.nome, payload['usuario']['nome'])
-        self.assertEqual(prod.usuario.cpf, payload['usuario']['cpf'])
-        self.assertEqual(prod.usuario.telefone, payload['usuario']['telefone'])
-        self.assertNotEqual(prod.password, payload['usuario']['senha'])
-        self.assertEqual(prod.dap, payload['dap'])
+        self.assertEqual(prod.usuario.nome, payload["usuario"]["nome"])
+        self.assertEqual(prod.usuario.cpf, payload["usuario"]["cpf"])
+        self.assertEqual(prod.usuario.telefone, payload["usuario"]["telefone"])
+        self.assertNotEqual(prod.usuario.password, payload["usuario"]["senha"])
+        self.assertEqual(prod.dap, payload["dap"])
