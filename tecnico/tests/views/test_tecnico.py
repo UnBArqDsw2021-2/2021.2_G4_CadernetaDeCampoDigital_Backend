69217bff89c09d59602df7f2046695bb75e4d612
Thiago Paiva <thiagohdaqw@gmail.com>
2022-03-10 21:20:11 -0300

    #105 adicionando testes de crea e usuario do tecnico

--- tecnico/tests/views/test_tecnico.py
+++ tecnico/tests/views/test_tecnico.py
@@ -1,25 +1,20 @@
-from core.tests.mixin import APITestMixin
 from django.test import TestCase
-
+from core.tests.mixin import APITestMixin
 from rest_framework.reverse import reverse_lazy
+from parameterized import parameterized
 
 from tecnico.models.tecnico import Tecnico
-
 from tecnico.tests.recipes import tecnico as tecnico_recipe
 
-class TecnicoAPIViewTest(APITestMixin, TestCase):
+from usuario.tests.views.usuario_base import UsuarioApiViewBase
+
+class TecnicoAPIViewTest(UsuarioApiViewBase, APITestMixin, TestCase):
     url = reverse_lazy("tecnico-create")
 
     def _payload(self):
         return {
-            "usuario": {
-                "cpf": "75610670039",
-                "dataNascimento": "2000-01-01",
-                "telefone": "5561912345678",
-                "nome": "Mendes Eduardo",
-                "senha": "senha_super_secreta",
-            },
-            "crea": "ABC123456",
+            "usuario": super()._payload(),
+            "crea": "1234567891",
             "formacao": "Engenheiro Agronomo",
             "email": "mendes.eduardo@email.com",
         }
@@ -42,11 +37,25 @@ class TecnicoAPIViewTest(APITestMixin, TestCase):
         self.assertEqual(tecnico.formacao, payload["formacao"])
         self.assertEqual(tecnico.email, payload["email"])
 
-    def test_cria_tecnico_crea_duplicado(self):
+    def test_nao_cria_tecnico_crea_duplicado(self):
         payload = self._payload()
         tecnico_recipe.make(crea=payload['crea'])
         
         response = self.client.post(self.url, payload, format='json')
 
         self.assertEqual(response.status_code, 400, response.json())
-        self.assertIn('tecnico com este crea já existe.', response.json()['crea'])
+        self.assertIn('Técnico já cadastrado.', response.json()['crea'])
+
+    @parameterized.expand([
+        ('123456789'),
+        ('ABC1234568',),
+        ('12345678912',),
+    ])
+    def test_nao_cria_tecnico_crea_invalido(self, crea):
+        payload = self._payload()
+        payload['crea'] = crea
+
+        response = self.client.post(self.url, payload, format='json')
+        
+        self.assertEqual(response.status_code, 400, response.json())
+        self.assertIn('Crea deve possuir 10 digitos.', response.json()['crea'])
