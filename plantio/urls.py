b3b608b01a5ddb7cc319f854d51336593bc93b07
Joao-Moura <jaummoura15@gmail.com>
2022-03-28 23:44:43 -0300

    #155 Adicionando novos endpoint de associação de plantio

--- plantio/urls.py
+++ plantio/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from django.urls import include
 
-from plantio.views.plantio import PlantioAPIView
+from plantio.views.plantio import PlantioAPIView, PlantioAssociacaoAPIView
 
 
 urlpatterns = [
     url(r'^plantio/', include([
         url(r'^$', PlantioAPIView.as_view(), name='plantio-create'),
+        url(
+            r'^(?P<idPlantio>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$',
+            PlantioAssociacaoAPIView.as_view(), name='plantio-associar'),
     ]))
 ]
--- plantio/views/plantio.py
+++ plantio/views/plantio.py
@@ -1,7 +1,9 @@
 from plantio.models import Plantio
-from plantio.serializers.plantio import PlantioSerializer
+from plantio.serializers.plantio import PlantioSerializer, PlantioAssociacaoSerializer
 
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView, get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class PlantioAPIView(CreateAPIView):
@@ -9,3 +11,17 @@ class PlantioAPIView(CreateAPIView):
 
     def get_queryset(self):
         return Plantio.objects.all()
+
+
+class PlantioAssociacaoAPIView(GenericAPIView):
+    lookup_field = ''
+    serializer_class = PlantioAssociacaoSerializer
+
+    def get_object(self):
+        return get_object_or_404(Plantio, idPlantio=self.kwargs['idPlantio'])
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, instance=self.get_object())
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
