1fe0b14515c5b24d390931cc6751a7a7ffc95552
Joao-Moura <jaummoura15@gmail.com>
2022-03-31 22:50:38 -0300

    #156 Adicionando endpoint de listagem (histórico) de plantios por propriedade
    Co-authored-by: Rafael Cleydson <rafael.cleydson@gmail.com>

--- plantio/urls.py
+++ plantio/urls.py
@@ -1,18 +1,23 @@
 from django.conf.urls import url
 from django.urls import include
 
-from plantio.views.plantio import PlantioAPIView
-from plantio.views.aplicacao import AplicacaoAgrotoxicoAPIView
+from plantio.views import plantio, aplicacao
 
 
 urlpatterns = [
     url(r'^plantio/', include([
-        url(r'^$', PlantioAPIView.as_view(), name='plantio-create'),
+        url(r'^$', plantio.PlantioAPIView.as_view(), name='plantio-create'),
 
         url(r'^aplicar/', include([
             url(
-                r'agrotoxico/$', AplicacaoAgrotoxicoAPIView.as_view(),
+                r'agrotoxico/$', aplicacao.AplicacaoAgrotoxicoAPIView.as_view(),
                 name='plantio-associar'),
+        ])),
+
+        url(r'^(?P<idPropriedade>[0-9A-Za-z]{8}-[0-9A-Za-z]{4}-4[0-9A-Za-z]{3}-[89ABab][0-9A-Za-z]{3}-[0-9A-Za-z]{12})/historico/', include([
+            url(
+                r'^propriedade/$', plantio.PlantioHistoricoPropriedadeAPIView.as_view(),
+                name='plantio-historico-propriedade'),
         ])),
     ]))
 ]
--- plantio/views/plantio.py
+++ plantio/views/plantio.py
@@ -1,7 +1,9 @@
 from plantio.models import Plantio
 from plantio.serializers.plantio import PlantioSerializer
 
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
+
+from talhao.models import Talhao
 
 
 class PlantioAPIView(CreateAPIView):
@@ -9,3 +11,12 @@ class PlantioAPIView(CreateAPIView):
 
     def get_queryset(self):
         return Plantio.objects.all()
+
+
+class PlantioHistoricoPropriedadeAPIView(ListAPIView):
+    serializer_class = PlantioSerializer
+    lookup_field = 'idPropriedade'
+
+    def get_queryset(self):
+        return Plantio.objects.filter(
+            talhao__in=Talhao.objects.filter(**self.kwargs).values_list('idTalhao'))
