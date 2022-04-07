2efd40cd6e64bafdd2adaec081c6c990c2ca16cf
Joao-Moura <jaummoura15@gmail.com>
2022-04-07 00:21:16 -0300

    #168 Adicionando endpoint de remoção de agrotóxicos

--- agrotoxico/views/agrotoxico.py
+++ agrotoxico/views/agrotoxico.py
@@ -1,7 +1,11 @@
 from agrotoxico.models import Agrotoxico
 from agrotoxico.serializers.agrotoxico import AgrotoxicoCreateSerializer, AgrotoxicoListSerializer
 
-from rest_framework.generics import ListCreateAPIView
+from plantio.models import AplicacaoAgrotoxico
+
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class AgrotoxicoAPIView(ListCreateAPIView):
@@ -11,3 +15,18 @@ class AgrotoxicoAPIView(ListCreateAPIView):
         if self.request.method.lower() == "get":
             return AgrotoxicoListSerializer
         return AgrotoxicoCreateSerializer
+
+
+class AgrotoxicoDestroyAPIView(DestroyAPIView):
+    queryset = Agrotoxico.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if AplicacaoAgrotoxico.objects.filter(agrotoxico=instance).exists():
+            return Response(
+                {'agrotoxico': 'Não é possível apagar um agrotoxico que já foi aplicado em um plantio.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
