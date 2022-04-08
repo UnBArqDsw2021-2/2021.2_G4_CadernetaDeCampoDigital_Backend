cf91e0f20bd5b87253e27f58ff34f6203a93a2e8
Rafael Ramos <rafael.cleydson@gmail.com>
2022-04-08 01:36:26 -0300

    #172 Escrevendo testes de comportamento detail/patch do plantio

--- plantio/tests/views/test_plantio.py
+++ plantio/tests/views/test_plantio.py
@@ -5,13 +5,15 @@ from datetime import date, timedelta
 from django.test import TestCase
 
 from core.tests.mixin import APITestMixin
-from core.consts.plantios import PLANTIO_CHOICES
-
+from core.consts.plantios import PLANTADO
 from rest_framework.reverse import reverse_lazy
 
 from parameterized import parameterized
 
 from plantio.models import Plantio
+from plantio.tests.recipes import plantio
+
+from propriedade.tests.recipes import propriedade
 
 from talhao.tests.recipes import talhao
 
@@ -28,7 +30,7 @@ class PlantioAPIViewTest(APITestMixin, TestCase):
             'talhao': self.talhao.idTalhao,
             'cultura': self.cultura.idCultura,
             'dataPlantio': date.today(),
-            'estado': PLANTIO_CHOICES[1][0]
+            'estado': PLANTADO
         }
 
     def test_cria_plantio(self):
@@ -80,3 +82,108 @@ class PlantioAPIViewTest(APITestMixin, TestCase):
         self.assertEqual(response.status_code, 400, response.json())
         self.assertIn(
             'Data de plantio no futuro.', response.json()['dataPlantio'])
+
+
+class PlantioRetrieveUpdateAPIViewTest(APITestMixin, TestCase):
+
+    def setUp(self):
+        self.propriedade = propriedade.make()
+        self.talhao = talhao.make(idPropriedade=self.propriedade)
+        self.plantio = plantio.make(talhao=self.talhao)
+        self.url = self.get_url()
+
+    def _payload(self):
+        return {
+            'talhao': talhao.make(idPropriedade=self.propriedade).idTalhao,
+            'cultura': cultura.make().idCultura,
+            'dataPlantio': date.today(),
+            'estado': PLANTADO
+        }
+
+    def get_url(self, idPlantio=None):
+        if idPlantio is None:
+            idPlantio = self.plantio.idPlantio
+        return reverse_lazy("plantio-detail-update", kwargs={'pk': idPlantio})
+
+    def test_detalha_plantio_existente(self):
+        response = self.client.get(self.url, format="json")
+        cultura_nome = response.json()['cultura']['nome']
+
+        self.assertEqual(response.status_code, 200, response.json())
+        self.assertEqual(5, len(response.json()))
+        self.assertEqual(str(self.plantio.idPlantio), response.json()["idPlantio"])
+        self.assertEqual(str(self.plantio.talhao.idTalhao), response.json()["talhao"])
+        self.assertEqual(str(self.plantio.dataPlantio), response.json()["dataPlantio"])
+        self.assertEqual(self.plantio.cultura.nome, cultura_nome)
+
+    def test_nao_detalha_plantio_inexistente(self):
+        url = self.get_url('00000000-0000-4000-8000-000000000000')
+
+        response = self.client.get(url, format="json")
+
+        self.assertEqual(response.status_code, 404, response.json())
+        self.assertIn('Não encontrado.', response.json()['detail'])
+
+    def test_atualiza_plantio(self):
+        payload = self._payload()
+
+        response = self.client.patch(self.url, data=payload, format="json")
+
+        self.assertEqual(response.status_code, 200, response.json())
+        self.assertEqual(Plantio.objects.count(), 1)
+
+        self.plantio.refresh_from_db()
+        self.assertEqual(self.plantio.cultura.idCultura, payload["cultura"])
+        self.assertEqual(self.plantio.talhao.idTalhao, payload["talhao"])
+        self.assertEqual(self.plantio.dataPlantio, payload["dataPlantio"])
+        self.assertEqual(self.plantio.estado, payload["estado"])
+
+    def test_nao_atualiza_plantio_inexistente(self):
+        payload = self._payload()
+        url = self.get_url('00000000-0000-4000-8000-000000000000')
+
+        response = self.client.patch(url, data=payload, format="json")
+
+        self.assertEqual(response.status_code, 404, response.json())
+        self.assertIn('Não encontrado.', response.json()['detail'])
+
+    def test_nao_atualiza_plantio_talhao_nao_existe_na_propriedade_do_plantio(self):
+        payload = self._payload()
+        payload['talhao'] = talhao.make().idTalhao
+
+        response = self.client.patch(self.url, data=payload, format="json")
+        error_message = 'Esse talhão não pertence a propriedade onde está localizado esse plantio.'
+
+        self.assertEqual(response.status_code, 400, response.json())
+        self.assertIn(error_message, response.json()['error'])
+
+    @parameterized.expand(['cultura', 'talhao'])
+    def test_nao_atualiza_plantio_cultura_talhao_inexistente(self, campo):
+        payload = self._payload()
+        payload[campo] = '00000000-0000-4000-8000-000000000000'
+
+        response = self.client.patch(self.url, data=payload, format="json")
+        error_message = f"Pk inválido \"{payload[campo]}\" - objeto não existe."
+
+        self.assertEqual(response.status_code, 400, response.json())
+        self.assertIn(error_message, response.json()[campo])
+
+    def test_nao_atualiza_plantio_data_futura(self):
+        payload = self._payload()
+        payload['dataPlantio'] = date.today() + timedelta(days=1)
+
+        response = self.client.patch(self.url, data=payload, format="json")
+        error_message = 'Data de plantio no futuro.'
+
+        self.assertEqual(response.status_code, 400, response.json())
+        self.assertIn(error_message, response.json()['dataPlantio'])
+
+    def test_nao_atualiza_estado_inexistente(self):
+        payload = self._payload()
+        payload['estado'] = 'Inexistente'
+
+        response = self.client.patch(self.url, data=payload, format="json")
+        error_message = f"\"{payload['estado']}\" não é um escolha válido."
+
+        self.assertEqual(response.status_code, 400, response.json())
+        self.assertIn(error_message, response.json()['estado'])
