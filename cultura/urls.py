fbe5e07067cabcd035b77a0e161588ca0471c9c6
Joao-Moura <jaummoura15@gmail.com>
2022-04-11 23:05:44 -0300

    #180 Modificando endpoint de culturas para listagem

--- cultura/urls.py
+++ cultura/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.urls import include
 
-from cultura.views.cultura import CulturaAPIView
+from cultura.views.cultura import CulturaListCreateAPIView
 from cultura.views.espera import CulturaEsperaAgrotoxicoAPIView
 
 
 urlpatterns = [
     url(r'^cultura/', include([
-        url(r'^$', CulturaAPIView.as_view(), name='cultura-create'),
+        url(r'^$', CulturaListCreateAPIView.as_view(), name='cultura-list-create'),
         url(r'^espera/agrotoxico/$', CulturaEsperaAgrotoxicoAPIView.as_view(), name='cultura-espera-agrotoxico')
     ]))
 ]
